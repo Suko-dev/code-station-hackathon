e89a960daf437a6e5561eaf5213f50a6c2883aba
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-06-16 18:59:54 -0300

    Remove packege.lick.json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,11 @@
 import express from "express";
 
+import { router } from "./routes";
+
 const app = express();
+
+app.use(express.json());
+
+app.use(router);
 
 app.listen(8080, () => console.log(`Servidor rodando na porta 8080`));
